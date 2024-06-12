2597e8ae2fb33cfe07b02db6393a27dd9faacdcf
swe-bench <>
2024-06-12 16:16:59 -0400

    Task instance Python/1

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from typing import List
 
 
-def has_close_elements(numbers: List[float], threshold: float) -> bool:
-    """ Check if in given list of numbers, are any two numbers closer to each other than
-    given threshold.
-    >>> has_close_elements([1.0, 2.0, 3.0], 0.5)
-    False
-    >>> has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3)
-    True
+def separate_paren_groups(paren_string: str) -> List[str]:
+    """ Input to this function is a string containing multiple groups of nested parentheses. Your goal is to
+    separate those group into separate strings and return the list of those.
+    Separate groups are balanced (each open brace is properly closed) and not nested within each other
+    Ignore any spaces in the input string.
+    >>> separate_paren_groups('( ) (( )) (( )( ))')
+    ['()', '(())', '(()())']
     """
-    for idx, elem in enumerate(numbers):
-        for idx2, elem2 in enumerate(numbers):
-            if idx != idx2:
-                distance = elem - elem2
-                if distance < threshold:
-                    return True
+    result = []
+    current_string = []
+    current_depth = 0
 
-    return False
-def check(has_close_elements):
-    assert has_close_elements([1.0, 2.0, 3.0], 0.5) == False
-    assert has_close_elements([1.0, 2.8, 3.0, 4.0, 5.0, 2.0], 0.3) == True
-check(has_close_elements)
+    for c in paren_string:
+        if c == '(':
+            current_depth += 1
+            current_string.append(c)
+        elif c == ')':
+            current_depth -= 1
+            current_string.append(c)
+
+            if current_depth < 0:
+                result.append(''.join(current_string))
+                current_string.clear()
+
+    return result
+def check(separate_paren_groups):
+    assert separate_paren_groups('( ) (( )) (( )( ))') == ['()', '(())', '(()())']
+check(separate_paren_groups)
