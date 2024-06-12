22dd8c28de599bcd8edb939fe80daccf8e16a7b5
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/111

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 
-def exchange(lst1, lst2):
-    """In this problem, you will implement a function that takes two lists of numbers,
-    and determines whether it is possible to perform an exchange of elements
-    between them to make lst1 a list of only even numbers.
-    There is no limit on the number of exchanged elements between lst1 and lst2.
-    If it is possible to exchange elements between the lst1 and lst2 to make
-    all the elements of lst1 to be even, return "YES".
-    Otherwise, return "NO".
-    For example:
-    exchange([1, 2, 3, 4], [1, 2, 3, 4]) => "YES"
-    exchange([1, 2, 3, 4], [1, 5, 3, 4]) => "NO"
-    It is assumed that the input lists will be non-empty.
+def histogram(test):
+    """Given a string representing a space separated lowercase letters, return a dictionary
+    of the letter with the most repetition and containing the corresponding count.
+    If several letters have the same occurrence, return all of them.
+    
+    Example:
+    histogram('a b c') == {'a': 1, 'b': 1, 'c': 1}
+    histogram('a b b a') == {'a': 2, 'b': 2}
+    histogram('a b c a b') == {'a': 2, 'b': 2}
+    histogram('b b b b a') == {'b': 4}
+    histogram('') == {}
+
     """
-    odd = 0
-    even = 0
-    for i in lst1:
-        if i%2 == 1:
-            even -= 1
-    for i in lst2:
-        if i%2 == 0:
-            odd += 1
-    if even >= odd:
-        return "YES"
-    return "NO"
-            
-def check(exchange):
+    dict1={}
+    list1=test.split(" ")
+    t=1
+
+    for i in list1:
+        if(list1.count(i)>t) and i!='':
+            t=list1.count(i)
+    if t>0:
+        for i in list1:
+            if(list1.count(i)==t):
+                
+                dict1[i]=t
+    return dict1
+def check(histogram):
     # Check some simple cases
-    assert exchange([1, 2, 3, 4], [1, 2, 3, 4]) == "YES"
-    assert exchange([1, 2, 3, 4], [1, 5, 3, 4]) == "NO"
-check(exchange)
+    assert histogram('a b b a') == {'a':2,'b': 2}, "This prints if this assert fails 1 (good for debugging!)"
+    assert histogram('a b c a b') == {'a': 2, 'b': 2}, "This prints if this assert fails 2 (good for debugging!)"
+    assert histogram('a b c') == {'a': 1,'b': 1,'c': 1}, "This prints if this assert fails 4 (good for debugging!)"
+    assert histogram('b b b b a') == {'b': 4}, "This prints if this assert fails 5 (good for debugging!)"
+    # Check some edge cases that are easy to work out by hand.
+    assert histogram('') == {}, "This prints if this assert fails 7 (also good for debugging!)"
+check(histogram)
