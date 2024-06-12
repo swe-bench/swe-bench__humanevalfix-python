40f1fc12c911e291eda147604e3caeb4cb2eabaa
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/107

--- main.py
+++ main.py
@@ -1,24 +1,41 @@
 
-def f(n):
-    """ Implement the function f that takes n as a parameter,
-    and returns a list of size n, such that the value of the element at index i is the factorial of i if i is even
-    or the sum of numbers from 1 to i otherwise.
-    i starts from 1.
-    the factorial of i is the multiplication of the numbers from 1 to i (1 * 2 * ... * i).
-    Example:
-    f(5) == [1, 2, 6, 24, 15]
+def even_odd_palindrome(n):
     """
-    ret = []
-    for i in range(1,n+1):
-        if i%2 == 0:
-            x = 1
-            for j in range(1,i+1): x *= i
-            ret += [x]
-        else:
-            x = 0
-            for j in range(1,i+1): x += j
-            ret += [x]
-    return ret
-def check(f):
-    assert f(5) == [1, 2, 6, 24, 15]
-check(f)
+    Given a positive integer n, return a tuple that has the number of even and odd
+    integer palindromes that fall within the range(1, n), inclusive.
+
+    Example 1:
+
+        Input: 3
+        Output: (1, 2)
+        Explanation:
+        Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
+
+    Example 2:
+
+        Input: 12
+        Output: (4, 6)
+        Explanation:
+        Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
+
+    Note:
+        1. 1 <= n <= 10^3
+        2. returned tuple has the number of even and odd integer palindromes respectively.
+    """
+    def is_palindrome(n):
+        return str(n) == str(n)[::-1]
+
+    even_palindrome_count = 0
+    odd_palindrome_count = 0
+
+    for i in range(1, n):
+        if i%2 == 1 and is_palindrome(i):
+                odd_palindrome_count += 1
+        elif i%2 == 0 and is_palindrome(i):
+            even_palindrome_count += 1
+    return (even_palindrome_count, odd_palindrome_count)
+def check(even_odd_palindrome):
+    # Check some simple cases
+    assert even_odd_palindrome(12) == (4, 6)
+    assert even_odd_palindrome(3) == (1, 2)
+check(even_odd_palindrome)
