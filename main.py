34756309c3df331a954cb7fa64cc884e3c87b17a
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/103

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 
-def choose_num(x, y):
-    """This function takes two positive numbers x and y and returns the
-    biggest even integer number that is in the range [x, y] inclusive. If 
-    there's no such number, then the function should return -1.
-
-    For example:
-    choose_num(12, 15) = 14
-    choose_num(13, 12) = -1
+def rounded_avg(n, m):
+    """You are given two positive integers n and m, and your task is to compute the
+    average of the integers from n through m (including n and m). 
+    Round the answer to the nearest integer and convert that to binary.
+    If n is greater than m, return -1.
+    Example:
+    rounded_avg(1, 5) => "0b11"
+    rounded_avg(7, 5) => -1
+    rounded_avg(10, 20) => "0b1111"
+    rounded_avg(20, 33) => "0b11010"
     """
-    if x > y:
+    if m < n:
         return -1
-    if y % 2 == 0:
-        return y
-    if x == y:
-        return -1
-    return x - 1
-def check(choose_num):
+    summation = 0
+    for i in range(n, m+1):
+        summation += i
+    return bin(round(summation/(m - n)))
+def check(rounded_avg):
     # Check some simple cases
-    assert choose_num(12, 15) == 14
-    assert choose_num(13, 12) == -1
-check(choose_num)
+    assert rounded_avg(1, 5) == "0b11"
+    # Check some edge cases that are easy to work out by hand.
+    assert rounded_avg(7, 5) == -1
+    assert rounded_avg(10,20) == "0b1111"
+    assert rounded_avg(20, 33) == "0b11010"
+check(rounded_avg)
