6c271f6afca57034da711bc771bb5a3940e2f1cf
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/110

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 
-def move_one_ball(arr):
-    """We have an array 'arr' of N integers arr[1], arr[2], ..., arr[N].The
-    numbers in the array will be randomly ordered. Your task is to determine if
-    it is possible to get an array sorted in non-decreasing order by performing 
-    the following operation on the given array:
-        You are allowed to perform right shift operation any number of times.
-    
-    One right shift operation means shifting all elements of the array by one
-    position in the right direction. The last element of the array will be moved to
-    the starting position in the array i.e. 0th index. 
-
-    If it is possible to obtain the sorted array by performing the above operation
-    then return True else return False.
-    If the given array is empty then return True.
-
-    Note: The given list is guaranteed to have unique elements.
-
-    For Example:
-    
-    move_one_ball([3, 4, 5, 1, 2])==>True
-    Explanation: By performin 2 right shift operations, non-decreasing order can
-                 be achieved for the given array.
-    move_one_ball([3, 5, 4, 1, 2])==>False
-    Explanation:It is not possible to get non-decreasing order for the given
-                array by performing any number of right shift operations.
-                
+def exchange(lst1, lst2):
+    """In this problem, you will implement a function that takes two lists of numbers,
+    and determines whether it is possible to perform an exchange of elements
+    between them to make lst1 a list of only even numbers.
+    There is no limit on the number of exchanged elements between lst1 and lst2.
+    If it is possible to exchange elements between the lst1 and lst2 to make
+    all the elements of lst1 to be even, return "YES".
+    Otherwise, return "NO".
+    For example:
+    exchange([1, 2, 3, 4], [1, 2, 3, 4]) => "YES"
+    exchange([1, 2, 3, 4], [1, 5, 3, 4]) => "NO"
+    It is assumed that the input lists will be non-empty.
     """
-    if len(arr)==0:
-      return True
-    sorted_array=sorted(arr)
-    my_arr=[]
-    
-    min_value=min(arr)
-    min_index=sorted_array.index(min_value)
-    my_arr=arr[min_index:]+arr[0:min_index]
-    for i in range(len(arr)):
-      if my_arr[i]!=sorted_array[i]:
-        return False
-    return True
-def check(move_one_ball):
+    odd = 0
+    even = 0
+    for i in lst1:
+        if i%2 == 1:
+            even -= 1
+    for i in lst2:
+        if i%2 == 0:
+            odd += 1
+    if even >= odd:
+        return "YES"
+    return "NO"
+            
+def check(exchange):
     # Check some simple cases
-    assert move_one_ball([3, 4, 5, 1, 2])==True, "This prints if this assert fails 1 (good for debugging!)"
-    # Check some edge cases that are easy to work out by hand.
-    assert move_one_ball([3, 5, 4, 1, 2])==False, "This prints if this assert fails 2 (also good for debugging!)"
-check(move_one_ball)
+    assert exchange([1, 2, 3, 4], [1, 2, 3, 4]) == "YES"
+    assert exchange([1, 2, 3, 4], [1, 5, 3, 4]) == "NO"
+check(exchange)
