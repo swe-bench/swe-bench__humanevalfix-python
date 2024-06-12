dc1605a175110a2edd01857eae8078f4de51d0b6
swe-bench <>
2024-06-12 16:17:02 -0400

    Task instance Python/41

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
 
 
-def triples_sum_to_zero(l: list):
+def car_race_collision(n: int):
     """
-    triples_sum_to_zero takes a list of integers as an input.
-    it returns True if there are three distinct elements in the list that
-    sum to zero, and False otherwise.
+    Imagine a road that's a perfectly straight infinitely long line.
+    n cars are driving left to right;  simultaneously, a different set of n cars
+    are driving right to left.   The two sets of cars start out being very far from
+    each other.  All cars move in the same speed.  Two cars are said to collide
+    when a car that's moving left to right hits a car that's moving right to left.
+    However, the cars are infinitely sturdy and strong; as a result, they continue moving
+    in their trajectory as if they did not collide.
 
-    >>> triples_sum_to_zero([1, 3, 5, 0])
-    False
-    >>> triples_sum_to_zero([1, 3, -2, 1])
-    True
-    >>> triples_sum_to_zero([1, 2, 3, 7])
-    False
-    >>> triples_sum_to_zero([2, 4, -5, 3, 9, 7])
-    True
-    >>> triples_sum_to_zero([1])
-    False
+    This function outputs the number of such collisions.
     """
-    for i in range(1, len(l)):
-        for j in range(i + 1, len(l)):
-            for k in range(j + 1, len(l)):
-                if l[i] + l[j] + l[k] == 0:
-                    return True
-    return False
-def check(triples_sum_to_zero):
-    assert triples_sum_to_zero([1, 3, 5, 0]) == False
-    assert triples_sum_to_zero([1, 3, -2, 1]) == True
-    assert triples_sum_to_zero([1, 2, 3, 7]) == False
-    assert triples_sum_to_zero([2, 4, -5, 3, 9, 7]) == True
-check(triples_sum_to_zero)
+    return n**3
