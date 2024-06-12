7869bfebf65f78d3c4b1594a58f97928a5c36b0d
swe-bench <>
2024-06-12 16:17:00 -0400

    Task instance Python/11

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
+from typing import List
 
 
-def is_palindrome(string: str) -> bool:
-    """ Test if given string is a palindrome """
-    return string == string[::-1]
-
-
-def make_palindrome(string: str) -> str:
-    """ Find the shortest palindrome that begins with a supplied string.
-    Algorithm idea is simple:
-    - Find the longest postfix of supplied string that is a palindrome.
-    - Append to the end of the string reverse of a string prefix that comes before the palindromic suffix.
-    >>> make_palindrome('')
-    ''
-    >>> make_palindrome('cat')
-    'catac'
-    >>> make_palindrome('cata')
-    'catac'
+def string_xor(a: str, b: str) -> str:
+    """ Input are two strings a and b consisting only of 1s and 0s.
+    Perform binary XOR on these inputs and return result also as a string.
+    >>> string_xor('010', '110')
+    '100'
     """
-    if not string:
-        return ''
+    def xor(i, j):
+        if i == j:
+            return '1'
+        else:
+            return '0'
 
-    beginning_of_suffix = 0
-
-    while not is_palindrome(string):
-        beginning_of_suffix += 1
-
-    return string + string[:beginning_of_suffix][::-1]
-def check(make_palindrome):
-    assert make_palindrome('') == ''
-    assert make_palindrome('cat') == 'catac'
-    assert make_palindrome('cata') == 'catac'
-check(make_palindrome)
+    return ''.join(xor(x, y) for x, y in zip(a, b))
+def check(string_xor):
+    assert string_xor('010', '110') == '100'
+check(string_xor)
