8016ea84bc162190fe859fe616b1187ae77198f8
swe-bench <>
2024-06-12 16:17:01 -0400

    Task instance Python/26 with tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import remove_duplicates
+
+
+
+
+
+def check(remove_duplicates):
+    assert remove_duplicates([]) == []
+    assert remove_duplicates([1, 2, 3, 4]) == [1, 2, 3, 4]
+    assert remove_duplicates([1, 2, 3, 2, 4, 3, 5]) == [1, 4, 5]
+
+check(remove_duplicates)
