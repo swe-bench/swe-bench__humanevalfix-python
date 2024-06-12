8b60bfa3f4cbc3a07b18ad37655ce3a9704c3660
swe-bench <>
2024-06-12 16:17:09 -0400

    Task instance Python/145

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 
-def simplify(x, n):
-    """Your task is to implement a function that will simplify the expression
-    x * n. The function returns True if x * n evaluates to a whole number and False
-    otherwise. Both x and n, are string representation of a fraction, and have the following format,
-    <numerator>/<denominator> where both numerator and denominator are positive whole numbers.
-
-    You can assume that x, and n are valid fractions, and do not have zero as denominator.
-
-    simplify("1/5", "5/1") = True
-    simplify("1/6", "2/1") = False
-    simplify("7/10", "10/2") = False
+def order_by_points(nums):
     """
-    a, b = x.split("/")
-    c, d = n.split("/")
-    a = int(b) * int(c)
-    d = int(c) * int(b)
-    numerator = int(a) * int(c)
-    denom = int(b) * int(d)
-    if (numerator/denom == int(numerator/denom)):
-        return True
-    return False
-def check(simplify):
+    Write a function which sorts the given list of integers
+    in ascending order according to the sum of their digits.
+    Note: if there are several items with similar sum of their digits,
+    order them based on their index in original list.
+
+    For example:
+    >>> order_by_points([1, 11, -1, -11, -12]) == [-1, -11, 1, -12, 11]
+    >>> order_by_points([]) == []
+    """
+    def digits_sum(n):
+        neg = 1
+        if n < 0: n, neg = -1 * n, -1 + n 
+        n = [int(i) for i in str(n)]
+        n[0] = n[0] * neg
+        return sum(n)
+    return sorted(nums, key=digits_sum)
+def check(order_by_points):
     # Check some simple cases
-    assert simplify("1/5", "5/1") == True, 'test1'
-    assert simplify("1/6", "2/1") == False, 'test2'
-    assert simplify("7/10", "10/2") == False, 'test4'
-check(simplify)
+    assert order_by_points([1, 11, -1, -11, -12]) == [-1, -11, 1, -12, 11]
+    assert order_by_points([]) == []
+check(order_by_points)
