5ffc9321e00b9c86d14790eacae1f08f2e987098
swe-bench <>
2024-06-12 16:17:01 -0400

    Task instance Python/28 with tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import concatenate
+
+
+
+
+
+def check(concatenate):
+    assert concatenate([]) == ''
+    assert concatenate(['x', 'y', 'z']) == 'xyz'
+    assert concatenate(['x', 'y', 'z', 'w', 'k']) == 'xyzwk'
+
+check(concatenate)
