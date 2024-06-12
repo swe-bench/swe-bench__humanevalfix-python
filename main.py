b63d13d24ffcf039e0f154ab163373ce11463034
swe-bench <>
2024-06-12 16:17:02 -0400

    Task instance Python/46

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 
 
-def triangle_area(a, h):
-    """Given length of a side and high return area for a triangle.
-    >>> triangle_area(5, 3)
-    7.5
+def fib4(n: int):
+    """The Fib4 number sequence is a sequence similar to the Fibbonacci sequnece that's defined as follows:
+    fib4(0) -> 0
+    fib4(1) -> 0
+    fib4(2) -> 2
+    fib4(3) -> 0
+    fib4(n) -> fib4(n-1) + fib4(n-2) + fib4(n-3) + fib4(n-4).
+    Please write a function to efficiently compute the n-th element of the fib4 number sequence.  Do not use recursion.
+    >>> fib4(5)
+    4
+    >>> fib4(6)
+    8
+    >>> fib4(7)
+    14
     """
-    return a * h / 0.5
-def check(triangle_area):
-    assert triangle_area(5, 3) == 7.5
-check(triangle_area)
+    results = [0, 0, 2, 0]
+    if n < 4:
+        return results[n]
+
+    for _ in range(4, n + 1):
+        results.append(results[-1] + results[-2] + results[-3] + results[-4])
+        results.pop(0)
+
+    return results[-2]
+def check(fib4):
+    assert fib4(5) == 4
+    assert fib4(6) == 8
+    assert fib4(7) == 14
+check(fib4)
