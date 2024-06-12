413f20624a7d1b0100c0c4387d6236ea2f8c8c7b
swe-bench <>
2024-06-12 16:17:08 -0400

    Task instance Python/137

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 
-def largest_smallest_integers(lst):
-    '''
-    Create a function that returns a tuple (a, b), where 'a' is
-    the largest of negative integers, and 'b' is the smallest
-    of positive integers in a list.
-    If there is no negative or positive integers, return them as None.
+def compare_one(a, b):
+    """
+    Create a function that takes integers, floats, or strings representing
+    real numbers, and returns the larger variable in its given variable type.
+    Return None if the values are equal.
+    Note: If a real number is represented as a string, the floating point might be . or ,
 
-    Examples:
-    largest_smallest_integers([2, 4, 1, 3, 5, 7]) == (None, 1)
-    largest_smallest_integers([]) == (None, None)
-    largest_smallest_integers([0]) == (None, None)
-    '''
-    smallest = list(filter(lambda x: x < 0, lst))
-    largest = list(filter(lambda x: x > 0, lst))
-    largest = list(filter(lambda x: x > 0, smallest))
-    smallest = list(filter(lambda x: x > 0, largest))
-    return (max(smallest) if smallest else None, min(largest) if largest else None)
-def check(largest_smallest_integers):
+    compare_one(1, 2.5) ➞ 2.5
+    compare_one(1, "2,3") ➞ "2,3"
+    compare_one("5,1", "6") ➞ "6"
+    compare_one("1", 1) ➞ None
+    """
+    temp_a, temp_b = a, b
+    if isinstance(temp_a, str): temp_a = temp_a.replace(',','.').replace('.',',')
+    if isinstance(temp_b, str): temp_b = temp_b.replace(',','.')
+    if float(temp_a) == float(temp_b): return None
+    return a if float(temp_a) > float(temp_b) else b 
+def check(compare_one):
     # Check some simple cases
-    assert largest_smallest_integers([2, 4, 1, 3, 5, 7]) == (None, 1)
-    assert largest_smallest_integers([]) == (None, None)
-    assert largest_smallest_integers([0]) == (None, None)
+    assert compare_one(1, 2.5) == 2.5
+    assert compare_one(1, "2,3") == "2,3"
+    assert compare_one("5,1", "6") == "6"
+    assert compare_one("1", 1) == None
     # Check some edge cases that are easy to work out by hand.
     assert True
-check(largest_smallest_integers)
+check(compare_one)
