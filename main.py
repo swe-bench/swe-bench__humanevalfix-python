9935d23ff9ec6f7c279fca5b0a86e3b7056fa29a
swe-bench <>
2024-06-12 16:17:08 -0400

    Task instance Python/130

--- main.py
+++ main.py
@@ -1,64 +1,31 @@
 
-def minPath(grid, k):
-    """
-    Given a grid with N rows and N columns (N >= 2) and a positive integer k, 
-    each cell of the grid contains a value. Every integer in the range [1, N * N]
-    inclusive appears exactly once on the cells of the grid.
-
-    You have to find the minimum path of length k in the grid. You can start
-    from any cell, and in each step you can move to any of the neighbor cells,
-    in other words, you can go to cells which share an edge with you current
-    cell.
-    Please note that a path of length k means visiting exactly k cells (not
-    necessarily distinct).
-    You CANNOT go off the grid.
-    A path A (of length k) is considered less than a path B (of length k) if
-    after making the ordered lists of the values on the cells that A and B go
-    through (let's call them lst_A and lst_B), lst_A is lexicographically less
-    than lst_B, in other words, there exist an integer index i (1 <= i <= k)
-    such that lst_A[i] < lst_B[i] and for any j (1 <= j < i) we have
-    lst_A[j] = lst_B[j].
-    It is guaranteed that the answer is unique.
-    Return an ordered list of the values on the cells that the minimum path go through.
-
+def tri(n):
+    """Everyone knows Fibonacci sequence, it was studied deeply by mathematicians in 
+    the last couple centuries. However, what people don't know is Tribonacci sequence.
+    Tribonacci sequence is defined by the recurrence:
+    tri(1) = 3
+    tri(n) = 1 + n / 2, if n is even.
+    tri(n) =  tri(n - 1) + tri(n - 2) + tri(n + 1), if n is odd.
+    For example:
+    tri(2) = 1 + (2 / 2) = 2
+    tri(4) = 3
+    tri(3) = tri(2) + tri(1) + tri(4)
+           = 2 + 3 + 3 = 8 
+    You are given a non-negative integer number n, you have to a return a list of the 
+    first n + 1 numbers of the Tribonacci sequence.
     Examples:
-
-        Input: grid = [ [1,2,3], [4,5,6], [7,8,9]], k = 3
-        Output: [1, 2, 1]
-
-        Input: grid = [ [5,9,3], [4,1,6], [7,8,2]], k = 1
-        Output: [1]
+    tri(3) = [1, 3, 2, 8]
     """
-    n = len(grid)
-    val = n * n + 1
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == 1:
-                temp = []
-                if i != 0:
-                    temp.append(grid[i][j])
-
-                if j != 0:
-                    temp.append(grid[i][j])
-
-                if i != n - 1:
-                    temp.append(grid[i][j])
-
-                if j != n - 1:
-                    temp.append(grid[i][j])
-
-                val = min(temp)
-
-    ans = []
-    for i in range(k):
+    if n == 0:
+        return [1]
+    my_tri = [1, 3]
+    for i in range(2, n + 1):
         if i % 2 == 0:
-            ans.append(1)
+            my_tri.append(i / 2 + 1)
         else:
-            ans.append(val)
-    return ans
-def check(minPath):
+            my_tri.append(my_tri[i - 1] + my_tri[i - 2] + i + (i + 3) / 2)
+    return my_tri
+def check(tri):
     # Check some simple cases
-    print
-    assert minPath([[1, 2, 3], [4, 5, 6], [7, 8, 9]], 3) == [1, 2, 1]
-    assert minPath([[5, 9, 3], [4, 1, 6], [7, 8, 2]], 1) == [1]
-check(minPath)
+    assert tri(3) == [1, 3, 2.0, 8.0]
+check(tri)
