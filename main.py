69a1b254650822342de6eb5aa1af47f12fe20b88
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/112

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 
-def histogram(test):
-    """Given a string representing a space separated lowercase letters, return a dictionary
-    of the letter with the most repetition and containing the corresponding count.
-    If several letters have the same occurrence, return all of them.
-    
-    Example:
-    histogram('a b c') == {'a': 1, 'b': 1, 'c': 1}
-    histogram('a b b a') == {'a': 2, 'b': 2}
-    histogram('a b c a b') == {'a': 2, 'b': 2}
-    histogram('b b b b a') == {'b': 4}
-    histogram('') == {}
-
+def reverse_delete(s,c):
+    """Task
+    We are given two strings s and c, you have to deleted all the characters in s that are equal to any character in c
+    then check if the result string is palindrome.
+    A string is called palindrome if it reads the same backward as forward.
+    You should return a tuple containing the result string and True/False for the check.
+    Example
+    For s = "abcde", c = "ae", the result should be ('bcd',False)
+    For s = "abcdef", c = "b"  the result should be ('acdef',False)
+    For s = "abcdedcba", c = "ab", the result should be ('cdedc',True)
     """
-    dict1={}
-    list1=test.split(" ")
-    t=1
-
-    for i in list1:
-        if(list1.count(i)>t) and i!='':
-            t=list1.count(i)
-    if t>0:
-        for i in list1:
-            if(list1.count(i)==t):
-                
-                dict1[i]=t
-    return dict1
-def check(histogram):
-    # Check some simple cases
-    assert histogram('a b b a') == {'a':2,'b': 2}, "This prints if this assert fails 1 (good for debugging!)"
-    assert histogram('a b c a b') == {'a': 2, 'b': 2}, "This prints if this assert fails 2 (good for debugging!)"
-    assert histogram('a b c') == {'a': 1,'b': 1,'c': 1}, "This prints if this assert fails 4 (good for debugging!)"
-    assert histogram('b b b b a') == {'b': 4}, "This prints if this assert fails 5 (good for debugging!)"
-    # Check some edge cases that are easy to work out by hand.
-    assert histogram('') == {}, "This prints if this assert fails 7 (also good for debugging!)"
-check(histogram)
+    s = ''.join([char for char in s if char not in c])
+    return (s,s[::-1] != s)
+def check(reverse_delete):
+    assert reverse_delete("abcde","ae") == ('bcd',False)
+    assert reverse_delete("abcdef", "b") == ('acdef',False)
+    assert reverse_delete("abcdedcba","ab") == ('cdedc',True)
+check(reverse_delete)
