ba92025c1fac35a001a77e1a9b995761624771a9
swe-bench <>
2024-06-12 16:17:02 -0400

    Task instance Python/45

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 
 
-def change_base(x: int, base: int):
-    """Change numerical base of input number x to base.
-    return string representation after the conversion.
-    base numbers are less than 10.
-    >>> change_base(8, 3)
-    '22'
-    >>> change_base(8, 2)
-    '1000'
-    >>> change_base(7, 2)
-    '111'
+def triangle_area(a, h):
+    """Given length of a side and high return area for a triangle.
+    >>> triangle_area(5, 3)
+    7.5
     """
-    ret = ""
-    while x > 0:
-        ret = str(x % base) + ret
-        x -= base
-    return ret
-def check(change_base):
-    assert change_base(8, 3) == "22"
-    assert change_base(8, 2) == "1000"
-    assert change_base(7, 2) == "111"
-check(change_base)
+    return a * h / 0.5
+def check(triangle_area):
+    assert triangle_area(5, 3) == 7.5
+check(triangle_area)
