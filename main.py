611648032507ef967e7342b94912d5ce7e2a62ff
swe-bench <>
2024-06-12 16:17:01 -0400

    Task instance Python/34

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 
 
-def sort_third(l: list):
-    """This function takes a list l and returns a list l' such that
-    l' is identical to l in the indicies that are not divisible by three, while its values at the indicies that are divisible by three are equal
-    to the values of the corresponding indicies of l, but sorted.
-    >>> sort_third([1, 2, 3])
-    [1, 2, 3]
-    >>> sort_third([5, 6, 3, 4, 8, 9, 2])
-    [2, 6, 3, 4, 8, 9, 5]
+def unique(l: list):
+    """Return sorted unique elements in a list
+    >>> unique([5, 3, 5, 2, 3, 3, 9, 0, 123])
+    [0, 2, 3, 5, 9, 123]
     """
-    l = list(l)
-    return l
-def check(sort_third):
-    assert sort_third([1, 2, 3]) == [1, 2, 3]
-    assert sort_third([5, 6, 3, 4, 8, 9, 2]) == [2, 6, 3, 4, 8, 9, 5]
-check(sort_third)
+    return sorted(l)
+def check(unique):
+    assert unique([5, 3, 5, 2, 3, 3, 9, 0, 123]) == [0, 2, 3, 5, 9, 123]
+check(unique)
