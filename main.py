78491286f8359b796c8a35aa3205753aada632fa
swe-bench <>
2024-06-12 16:17:08 -0400

    Task instance Python/140

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 
-def special_factorial(n):
-    """The Brazilian factorial is defined as:
-    brazilian_factorial(n) = n! * (n-1)! * (n-2)! * ... * 1!
-    where n > 0
-
-    For example:
-    >>> special_factorial(4)
-    288
-
-    The function will receive an integer as input and should return the special
-    factorial of this integer.
+def fix_spaces(text):
     """
-    fact_i = 1
-    special_fact = 1
-    for i in range(1, n+1):
-        i *= n
-        fact_i *= i
-        special_fact *= fact_i
-    return special_fact
-def check(special_factorial):
+    Given a string text, replace all spaces in it with underscores, 
+    and if a string has more than 2 consecutive spaces, 
+    then replace all consecutive spaces with - 
+    
+    fix_spaces("Example") == "Example"
+    fix_spaces("Example 1") == "Example_1"
+    fix_spaces(" Example 2") == "_Example_2"
+    fix_spaces(" Example   3") == "_Example-3"
+    """
+    new_text = ""
+    i = 0
+    start, end = 0, 0
+    while i < len(text):
+        if text[i] == " ":
+            end += 1
+        else:
+            if end - start > 2:
+                new_text += "-"+text[i]
+            elif end - start > 0:
+                new_text += "_"*(end - start)+text[i]
+            else:
+                new_text += text[i]
+            start, end = i+1, i+1
+        i+=1
+    if end - start > 2:
+        new_text += "-"
+    elif end - start > 0:
+        new_text += "__"
+    return new_text
+def check(fix_spaces):
     # Check some simple cases
-    assert special_factorial(4) == 288, "Test 4"
-check(special_factorial)
+    assert fix_spaces("Example") == "Example", "This prints if this assert fails 1 (good for debugging!)"
+    assert fix_spaces("Example 1") == "Example_1"
+    assert fix_spaces(" Example 2") == "_Example_2"
+    # Check some edge cases that are easy to work out by hand.
+    assert fix_spaces(" Example   3") == "_Example-3"
+check(fix_spaces)
