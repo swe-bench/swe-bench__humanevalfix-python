e3f83c3c193603975bbab8e74b2e54d5ad815153
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/109

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
 
-def count_nums(arr):
+def move_one_ball(arr):
+    """We have an array 'arr' of N integers arr[1], arr[2], ..., arr[N].The
+    numbers in the array will be randomly ordered. Your task is to determine if
+    it is possible to get an array sorted in non-decreasing order by performing 
+    the following operation on the given array:
+        You are allowed to perform right shift operation any number of times.
+    
+    One right shift operation means shifting all elements of the array by one
+    position in the right direction. The last element of the array will be moved to
+    the starting position in the array i.e. 0th index. 
+
+    If it is possible to obtain the sorted array by performing the above operation
+    then return True else return False.
+    If the given array is empty then return True.
+
+    Note: The given list is guaranteed to have unique elements.
+
+    For Example:
+    
+    move_one_ball([3, 4, 5, 1, 2])==>True
+    Explanation: By performin 2 right shift operations, non-decreasing order can
+                 be achieved for the given array.
+    move_one_ball([3, 5, 4, 1, 2])==>False
+    Explanation:It is not possible to get non-decreasing order for the given
+                array by performing any number of right shift operations.
+                
     """
-    Write a function count_nums which takes an array of integers and returns
-    the number of elements which has a sum of digits > 0.
-    If a number is negative, then its first signed digit will be negative:
-    e.g. -123 has signed digits -1, 2, and 3.
-    >>> count_nums([]) == 0
-    >>> count_nums([-1, 11, -11]) == 1
-    >>> count_nums([1, 1, 2]) == 3
-    """
-    def digits_sum(n):
-        neg = 1
-        if n < 0: n, neg = -1 * n, -1 
-        n = [int(i) for i in str(n)]
-        n[0] = n[0] * neg * -1
-        return sum(n)
-    return len(list(filter(lambda x: x > 0, [digits_sum(i) for i in arr])))
-def check(count_nums):
+    if len(arr)==0:
+      return True
+    sorted_array=sorted(arr)
+    my_arr=[]
+    
+    min_value=min(arr)
+    min_index=sorted_array.index(min_value)
+    my_arr=arr[min_index:]+arr[0:min_index]
+    for i in range(len(arr)):
+      if my_arr[i]!=sorted_array[i]:
+        return False
+    return True
+def check(move_one_ball):
     # Check some simple cases
-    assert count_nums([]) == 0
-    assert count_nums([-1, 11, -11]) == 1
-    assert count_nums([1, 1, 2]) == 3
+    assert move_one_ball([3, 4, 5, 1, 2])==True, "This prints if this assert fails 1 (good for debugging!)"
     # Check some edge cases that are easy to work out by hand.
-    assert True, "This prints if this assert fails 2 (also good for debugging!)"
-check(count_nums)
+    assert move_one_ball([3, 5, 4, 1, 2])==False, "This prints if this assert fails 2 (also good for debugging!)"
+check(move_one_ball)
