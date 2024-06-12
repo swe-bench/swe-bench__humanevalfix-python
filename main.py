4e74f6e34dd93c80ac889a8ed63fa56c92aa3f77
swe-bench <>
2024-06-12 16:16:59 -0400

    Task instance Python/7

--- main.py
+++ main.py
@@ -1,27 +1,15 @@
 from typing import List
 
 
-def parse_nested_parens(paren_string: str) -> List[int]:
-    """ Input to this function is a string represented multiple groups for nested parentheses separated by spaces.
-    For each of the group, output the deepest level of nesting of parentheses.
-    E.g. (()()) has maximum two levels of nesting while ((())) has three.
-
-    >>> parse_nested_parens('(()()) ((())) () ((())()())')
-    [2, 3, 1, 3]
+def filter_by_substring(strings: List[str], substring: str) -> List[str]:
+    """ Filter an input list of strings only for ones that contain given substring
+    >>> filter_by_substring([], 'a')
+    []
+    >>> filter_by_substring(['abc', 'bacd', 'cde', 'array'], 'a')
+    ['abc', 'bacd', 'array']
     """
-    def parse_paren_group(s):
-        depth = 0
-        max_depth = 0
-        for c in s:
-            if c == '(':
-                depth += 1
-                max_depth = max(depth, max_depth)
-            else:
-                max_depth -= 1
-
-        return max_depth
-
-    return [parse_paren_group(x) for x in paren_string.split(' ') if x]
-def check(parse_nested_parens):
-    assert parse_nested_parens('(()()) ((())) () ((())()())') == [2, 3, 1, 3]
-check(parse_nested_parens)
+    return [x for x in strings if x in substring]
+def check(filter_by_substring):
+    assert filter_by_substring([], 'a') == []
+    assert filter_by_substring(['abc', 'bacd', 'cde', 'array'], 'a') == ['abc', 'bacd', 'array']
+check(filter_by_substring)
