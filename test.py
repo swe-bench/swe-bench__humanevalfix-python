900d4c16a037b7d2d10495731a64e58ba1177446
swe-bench <>
2024-06-12 16:17:03 -0400

    Task instance Python/54 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import same_chars
+
+
+
+
+
+def check(same_chars):
+    assert same_chars('eabcdzzzz', 'dddzzzzzzzddeddabc') == True
+    assert same_chars('abcd', 'dddddddabc') == True
+    assert same_chars('dddddddabc', 'abcd') == True
+    assert same_chars('eabcd', 'dddddddabc') == False
+    assert same_chars('abcd', 'dddddddabcf') == False
+    assert same_chars('eabcdzzzz', 'dddzzzzzzzddddabc') == False
+    assert same_chars('aabb', 'aaccc') == False
+
+check(same_chars)
