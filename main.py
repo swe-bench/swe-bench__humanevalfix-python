27e55189bec92bbf5ca473b85b2a3b4476dc3d21
swe-bench <>
2024-06-12 16:17:08 -0400

    Task instance Python/134

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 
+def check_if_last_char_is_a_letter(txt):
+    '''
+    Create a function that returns True if the last character
+    of a given string is an alphabetical character and is not
+    a part of a word, and False otherwise.
+    Note: "word" is a group of characters separated by space.
 
-def sum_squares(lst):
-    """You are given a list of numbers.
-    You need to return the sum of squared numbers in the given list,
-    round each element in the list to the upper int(Ceiling) first.
     Examples:
-    For lst = [1,2,3] the output should be 14
-    For lst = [1,4,9] the output should be 98
-    For lst = [1,3,5,7] the output should be 84
-    For lst = [1.4,4.2,0] the output should be 29
-    For lst = [-2.4,1,1] the output should be 6
-    
-
-    """
-    import math
-    squared = 0
-    for i in lst:
-        squared += math.ceil(i)*2
-    return squared
-def check(sum_squares):
+    check_if_last_char_is_a_letter("apple pie") ➞ False
+    check_if_last_char_is_a_letter("apple pi e") ➞ True
+    check_if_last_char_is_a_letter("apple pi e ") ➞ False
+    check_if_last_char_is_a_letter("") ➞ False 
+    '''
+ 
+    check = txt.split(' ')[-1]
+    return True if len(check) == 1 and (97 <= ord(check.upper()) <= 122) else False
+def check(check_if_last_char_is_a_letter):
     # Check some simple cases
-    assert sum_squares([1,2,3])==14, "This prints if this assert fails 1 (good for debugging!)"
-    assert sum_squares([1,4,9])==98, "This prints if this assert fails 1 (good for debugging!)"
-    assert sum_squares([1,3,5,7])==84, "This prints if this assert fails 1 (good for debugging!)"
-    assert sum_squares([1.4,4.2,0])==29, "This prints if this assert fails 1 (good for debugging!)"
-    assert sum_squares([-2.4,1,1])==6, "This prints if this assert fails 1 (good for debugging!)"
-check(sum_squares)
+    assert check_if_last_char_is_a_letter("apple pi e") == True
+    assert check_if_last_char_is_a_letter("") == False
+    assert check_if_last_char_is_a_letter("apple pie") == False
+    assert check_if_last_char_is_a_letter("apple pi e ") == False
+    # Check some edge cases that are easy to work out by hand.
+    assert True
+check(check_if_last_char_is_a_letter)
