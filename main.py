f3280239397b3e4dce6dbf7ce4896d5bf37917b8
swe-bench <>
2024-06-12 16:17:06 -0400

    Task instance Python/108

--- main.py
+++ main.py
@@ -1,41 +1,26 @@
 
-def even_odd_palindrome(n):
+def count_nums(arr):
     """
-    Given a positive integer n, return a tuple that has the number of even and odd
-    integer palindromes that fall within the range(1, n), inclusive.
-
-    Example 1:
-
-        Input: 3
-        Output: (1, 2)
-        Explanation:
-        Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
-
-    Example 2:
-
-        Input: 12
-        Output: (4, 6)
-        Explanation:
-        Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
-
-    Note:
-        1. 1 <= n <= 10^3
-        2. returned tuple has the number of even and odd integer palindromes respectively.
+    Write a function count_nums which takes an array of integers and returns
+    the number of elements which has a sum of digits > 0.
+    If a number is negative, then its first signed digit will be negative:
+    e.g. -123 has signed digits -1, 2, and 3.
+    >>> count_nums([]) == 0
+    >>> count_nums([-1, 11, -11]) == 1
+    >>> count_nums([1, 1, 2]) == 3
     """
-    def is_palindrome(n):
-        return str(n) == str(n)[::-1]
-
-    even_palindrome_count = 0
-    odd_palindrome_count = 0
-
-    for i in range(1, n):
-        if i%2 == 1 and is_palindrome(i):
-                odd_palindrome_count += 1
-        elif i%2 == 0 and is_palindrome(i):
-            even_palindrome_count += 1
-    return (even_palindrome_count, odd_palindrome_count)
-def check(even_odd_palindrome):
+    def digits_sum(n):
+        neg = 1
+        if n < 0: n, neg = -1 * n, -1 
+        n = [int(i) for i in str(n)]
+        n[0] = n[0] * neg * -1
+        return sum(n)
+    return len(list(filter(lambda x: x > 0, [digits_sum(i) for i in arr])))
+def check(count_nums):
     # Check some simple cases
-    assert even_odd_palindrome(12) == (4, 6)
-    assert even_odd_palindrome(3) == (1, 2)
-check(even_odd_palindrome)
+    assert count_nums([]) == 0
+    assert count_nums([-1, 11, -11]) == 1
+    assert count_nums([1, 1, 2]) == 3
+    # Check some edge cases that are easy to work out by hand.
+    assert True, "This prints if this assert fails 2 (also good for debugging!)"
+check(count_nums)
