f4efe7b7dbea496290c853318c2920e2bdc37b38
swe-bench <>
2024-06-12 16:17:02 -0400

    Task instance Python/49 with tests

---
+++ test.py
@@ -0,0 +1,16 @@
+from main import modp
+
+
+
+
+
+def check(modp):
+    assert modp(3, 5) == 3
+    assert modp(1101, 101) == 2
+    assert modp(0, 101) == 1
+    assert modp(3, 11) == 8
+    assert modp(100, 101) == 1
+    assert modp(30, 5) == 4
+    assert modp(31, 5) == 3
+
+check(modp)
