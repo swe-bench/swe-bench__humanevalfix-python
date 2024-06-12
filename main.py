a604a5f475780c68f54fd8bd1c7b2d191318e085
swe-bench <>
2024-06-12 16:17:05 -0400

    Task instance Python/90

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 
-def encrypt(s):
-    """Create a function encrypt that takes a string as an argument and
-    returns a string encrypted with the alphabet being rotated. 
-    The alphabet should be rotated in a manner such that the letters 
-    shift down by two multiplied to two places.
-    For example:
-    encrypt('hi') returns 'lm'
-    encrypt('asdfghjkl') returns 'ewhjklnop'
-    encrypt('gf') returns 'kj'
-    encrypt('et') returns 'ix'
+def next_smallest(lst):
     """
-    d = 'abcdefghijklmnopqrstuvwxyz'
-    out = ''
-    for c in s:
-        if c in d:
-            out += d[(d.index(c)+2*2) % 24]
-        else:
-            out += c
-    return out
-def check(encrypt):
+    You are given a list of integers.
+    Write a function next_smallest() that returns the 2nd smallest element of the list.
+    Return None if there is no such element.
+    
+    next_smallest([1, 2, 3, 4, 5]) == 2
+    next_smallest([5, 1, 4, 3, 2]) == 2
+    next_smallest([]) == None
+    next_smallest([1, 1]) == None
+    """
+    lst = sorted(set(lst))
+    return None if len(lst) < 3 else lst[1]
+def check(next_smallest):
     # Check some simple cases
-    assert encrypt('hi') == 'lm', "This prints if this assert fails 1 (good for debugging!)"
-    assert encrypt('asdfghjkl') == 'ewhjklnop', "This prints if this assert fails 1 (good for debugging!)"
-    assert encrypt('gf') == 'kj', "This prints if this assert fails 1 (good for debugging!)"
-    assert encrypt('et') == 'ix'
-check(encrypt)
+    assert next_smallest([1, 2, 3, 4, 5]) == 2
+    assert next_smallest([5, 1, 4, 3, 2]) == 2
+    assert next_smallest([]) == None
+    assert next_smallest([1, 1]) == None
+    # Check some edge cases that are easy to work out by hand.
+    assert True
+check(next_smallest)
