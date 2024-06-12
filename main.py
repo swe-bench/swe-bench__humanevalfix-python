695fa5e1283d6c0524f1902cf8d0cddc167d5360
swe-bench <>
2024-06-12 16:17:02 -0400

    Task instance Python/50

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 
 
-def modp(n: int, p: int):
-    """Return 2^n modulo p (be aware of numerics).
-    >>> modp(3, 5)
-    3
-    >>> modp(1101, 101)
-    2
-    >>> modp(0, 101)
-    1
-    >>> modp(3, 11)
-    8
-    >>> modp(100, 101)
-    1
+def encode_shift(s: str):
     """
-    ret = 0
-    for i in range(n):
-        ret = (2 * ret) % p
-    return ret
-def check(modp):
-    assert modp(3, 5) == 3
-    assert modp(1101, 101) == 2
-    assert modp(0, 101) == 1
-    assert modp(3, 11) == 8
-    assert modp(100, 101) == 1
-check(modp)
+    returns encoded string by shifting every character by 5 in the alphabet.
+    """
+    return "".join([chr(((ord(ch) + 5 - ord("a")) % 26) + ord("a")) for ch in s])
+
+
+def decode_shift(s: str):
+    """
+    takes as input string encoded with encode_shift function. Returns decoded string.
+    """
+    return "".join([chr(((ord(ch) - 5 - ord("a")) % 26) + ord(ch)) for ch in s])
