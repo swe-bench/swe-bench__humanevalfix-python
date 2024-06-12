7dc482ba86e13cea2ffba707ff59a1e3eb3eaa63
swe-bench <>
2024-06-12 16:17:07 -0400

    Task instance Python/114

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 
-def odd_count(lst):
-    """Given a list of strings, where each string consists of only digits, return a list.
-    Each element i of the output should be "the number of odd elements in the
-    string i of the input." where all the i's should be replaced by the number
-    of odd digits in the i'th string of the input.
-
-    >>> odd_count(['1234567'])
-    ["the number of odd elements 4n the str4ng 4 of the 4nput."]
-    >>> odd_count(['3',"11111111"])
-    ["the number of odd elements 1n the str1ng 1 of the 1nput.",
-     "the number of odd elements 8n the str8ng 8 of the 8nput."]
+def minSubArraySum(nums):
     """
-    res = []
-    for arr in lst:
-        n = sum(int(d)%2==1 for d in arr)
-        res.append("the number of odd elements " + str(n) + "n the str"+ str(n) +"ng "+ str(n) +" of "+ str(n) +" the "+ str(n) +"nput.")
-    return res
-def check(odd_count):
+    Given an array of integers nums, find the minimum sum of any non-empty sub-array
+    of nums.
+    Example
+    minSubArraySum([2, 3, 4, 1, 2, 4]) == 1
+    minSubArraySum([-1, -2, -3]) == -6
+    """
+    max_sum = 0
+    s = 0
+    for num in nums:
+        s += -num
+        if (s < 0):
+            s = 0
+        max_sum = max(s, max_sum)
+    if max_sum == 0:
+        max_sum = max(-i for i in nums)
+    min_sum = min(-i for i in nums)
+    return min_sum
+def check(minSubArraySum):
     # Check some simple cases
-    assert odd_count(['1234567']) == ["the number of odd elements 4n the str4ng 4 of the 4nput."], "Test 1"
-    assert odd_count(['3',"11111111"]) == ["the number of odd elements 1n the str1ng 1 of the 1nput.", "the number of odd elements 8n the str8ng 8 of the 8nput."], "Test 2"
-    # Check some edge cases that are easy to work out by hand.
-    assert True, "This prints if this assert fails 2 (also good for debugging!)"
-check(odd_count)
+    assert minSubArraySum([2, 3, 4, 1, 2, 4]) == 1, "This prints if this assert fails 1 (good for debugging!)"
+    assert minSubArraySum([-1, -2, -3]) == -6
+check(minSubArraySum)
