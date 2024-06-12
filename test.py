3dd6c3936f08944c6104520fc63026fb1e8c5018
swe-bench <>
2024-06-12 16:17:00 -0400

    Task instance Python/12 with tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import longest
+
+
+
+
+
+def check(longest):
+    assert longest([]) == None
+    assert longest(['x', 'y', 'z']) == 'x'
+    assert longest(['x', 'yyy', 'zzzz', 'www', 'kkkk', 'abc']) == 'zzzz'
+
+check(longest)
