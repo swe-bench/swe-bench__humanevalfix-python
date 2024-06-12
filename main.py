9e63f0922b49f89e417c58af0d1b17923b68f0fa
swe-bench <>
2024-06-12 16:17:05 -0400

    Task instance Python/87

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 
-def anti_shuffle(s):
+def get_row(lst, x):
     """
-    Write a function that takes a string and returns an ordered version of it.
-    Ordered version of string, is a string where all words (separated by space)
-    are replaced by a new word where all the characters arranged in
-    ascending order based on ascii value.
-    Note: You should keep the order of words and blank spaces in the sentence.
-
-    For example:
-    anti_shuffle('Hi') returns 'Hi'
-    anti_shuffle('hello') returns 'ehllo'
-    anti_shuffle('Hello World!!!') returns 'Hello !!!Wdlor'
+    You are given a 2 dimensional data, as a nested lists,
+    which is similar to matrix, however, unlike matrices,
+    each row may contain a different number of columns.
+    Given lst, and integer x, find integers x in the list,
+    and return list of tuples, [(x1, y1), (x2, y2) ...] such that
+    each tuple is a coordinate - (row, columns), starting with 0.
+    Sort coordinates initially by rows in ascending order.
+    Also, sort coordinates of the row by columns in descending order.
+    
+    Examples:
+    get_row([
+      [1,2,3,4,5,6],
+      [1,2,3,4,1,6],
+      [1,2,3,4,5,1]
+    ], 1) == [(0, 0), (1, 4), (1, 0), (2, 5), (2, 0)]
+    get_row([], 1) == []
+    get_row([[], [1], [1, 2, 3]], 3) == [(2, 2)]
     """
-    return ''.join([''.join(sorted(list(i))) for i in s.split(' ')])
-def check(anti_shuffle):
+    coords = [(j, i) for i in range(len(lst)) for j in range(len(lst[i])) if lst[i][j] == x]
+    return sorted(sorted(coords, key=lambda x: x[1], reverse=True), key=lambda x: x[0])
+def check(get_row):
     # Check some simple cases
-    assert anti_shuffle('Hi') == 'Hi'
-    assert anti_shuffle('hello') == 'ehllo'
-    assert anti_shuffle('Hello World!!!') == 'Hello !!!Wdlor'
-check(anti_shuffle)
+    assert get_row([
+        [1,2,3,4,5,6],
+        [1,2,3,4,1,6],
+        [1,2,3,4,5,1]
+    ], 1) == [(0, 0), (1, 4), (1, 0), (2, 5), (2, 0)]
+    assert get_row([], 1) == []
+    assert get_row([[], [1], [1, 2, 3]], 3) == [(2, 2)]
+    # Check some edge cases that are easy to work out by hand.
+    assert True
+check(get_row)
