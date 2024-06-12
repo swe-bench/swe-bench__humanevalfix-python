7e203f204253f6db20b52f9555ef91abe806bf6d
swe-bench <>
2024-06-12 16:17:03 -0400

    Task instance Python/64

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 
+FIX = """
+Add more test cases.
+"""
 
-def fibfib(n: int):
-    """The FibFib number sequence is a sequence similar to the Fibbonacci sequnece that's defined as follows:
-    fibfib(0) == 0
-    fibfib(1) == 0
-    fibfib(2) == 1
-    fibfib(n) == fibfib(n-1) + fibfib(n-2) + fibfib(n-3).
-    Please write a function to efficiently compute the n-th element of the fibfib number sequence.
-    >>> fibfib(1)
-    0
-    >>> fibfib(5)
-    4
-    >>> fibfib(8)
-    24
+def vowels_count(s):
+    """Write a function vowels_count which takes a string representing
+    a word as input and returns the number of vowels in the string.
+    Vowels in this case are 'a', 'e', 'i', 'o', 'u'. Here, 'y' is also a
+    vowel, but only when it is at the end of the given word.
+
+    Example:
+    >>> vowels_count("abcde")
+    2
+    >>> vowels_count("ACEDY")
+    3
     """
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    return fibfib(n - 1) + fibfib(n - 2) + fibfib(n - 3)
-def check(fibfib):
-    assert fibfib(1) == 0
-    assert fibfib(5) == 4
-    assert fibfib(8) == 24
-check(fibfib)
+    vowels = "aeiouyAEIOUY"
+    n_vowels = sum(c in vowels for c in s)
+    return n_vowels
+def check(vowels_count):
+    # Check some simple cases
+    assert vowels_count("abcde") == 2, "Test 6"
+    assert vowels_count("ACEDY") == 3, "Test 7"
+    # Check some edge cases that are easy to work out by hand.
+    assert True, "This prints if this assert fails 2 (also good for debugging!)"
+check(vowels_count)
