ce79b0b1cf6fb8951bb23de62fb551f63443c2f0
swe-bench <>
2024-06-12 16:17:01 -0400

    Task instance Python/27

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from typing import List
 
 
-def remove_duplicates(numbers: List[int]) -> List[int]:
-    """ From a list of integers, remove all elements that occur more than once.
-    Keep order of elements left the same as in the input.
-    >>> remove_duplicates([1, 2, 3, 2, 4])
-    [1, 3, 4]
+def flip_case(string: str) -> str:
+    """ For a given string, flip lowercase characters to uppercase and uppercase to lowercase.
+    >>> flip_case('Hello')
+    'hELLO'
     """
-    import collections
-    c = collections.Counter(numbers)
-    return [n for n in numbers if c[n] < 1]
-def check(remove_duplicates):
-    assert remove_duplicates([1, 2, 3,2, 4]) == [1, 3, 4]
-check(remove_duplicates)
+    return string.lower()
+def check(flip_case):
+    assert flip_case('Hello') == 'hELLO'
+check(flip_case)
