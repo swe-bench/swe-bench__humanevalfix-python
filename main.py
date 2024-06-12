62b01aea2d59f0259ebefec1b60ee7c21987557f
swe-bench <>
2024-06-12 16:17:08 -0400

    Task instance Python/133

--- main.py
+++ main.py
@@ -1,42 +1,28 @@
 
-def is_nested(string):
-    '''
-    Create a function that takes a string as input which contains only square brackets.
-    The function should return True if and only if there is a valid subsequence of brackets 
-    where at least one bracket in the subsequence is nested.
 
-    is_nested('[[]]') ➞ True
-    is_nested('[]]]]]]][[[[[]') ➞ False
-    is_nested('[][]') ➞ False
-    is_nested('[]') ➞ False
-    is_nested('[[][]]') ➞ True
-    is_nested('[[]][[') ➞ True
-    '''
-    opening_bracket_index = []
-    closing_bracket_index = []
-    for i in range(len(string)):
-        if string[i] == '(':
-            opening_bracket_index.append(i)
-        else:
-            closing_bracket_index.append(i)
-    closing_bracket_index.reverse()
-    cnt = 0
-    i = 0
-    l = len(closing_bracket_index)
-    for idx in opening_bracket_index:
-        if i < l and idx < closing_bracket_index[i]:
-            cnt += 1
-            i += 1
-    return cnt >= 2
-
+def sum_squares(lst):
+    """You are given a list of numbers.
+    You need to return the sum of squared numbers in the given list,
+    round each element in the list to the upper int(Ceiling) first.
+    Examples:
+    For lst = [1,2,3] the output should be 14
+    For lst = [1,4,9] the output should be 98
+    For lst = [1,3,5,7] the output should be 84
+    For lst = [1.4,4.2,0] the output should be 29
+    For lst = [-2.4,1,1] the output should be 6
     
-def check(is_nested):
+
+    """
+    import math
+    squared = 0
+    for i in lst:
+        squared += math.ceil(i)*2
+    return squared
+def check(sum_squares):
     # Check some simple cases
-    assert is_nested('[[]]') == True, "This prints if this assert fails 1 (good for debugging!)"
-    assert is_nested('[]]]]]]][[[[[]') == False
-    assert is_nested('[][]') == False
-    assert is_nested('[]') == False
-    assert is_nested('[[]][[') == True
-    assert is_nested('[[][]]') == True
-    # Check some edge cases that are easy to work out by hand.
-check(is_nested)
+    assert sum_squares([1,2,3])==14, "This prints if this assert fails 1 (good for debugging!)"
+    assert sum_squares([1,4,9])==98, "This prints if this assert fails 1 (good for debugging!)"
+    assert sum_squares([1,3,5,7])==84, "This prints if this assert fails 1 (good for debugging!)"
+    assert sum_squares([1.4,4.2,0])==29, "This prints if this assert fails 1 (good for debugging!)"
+    assert sum_squares([-2.4,1,1])==6, "This prints if this assert fails 1 (good for debugging!)"
+check(sum_squares)
