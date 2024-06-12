6ba76cfb68f9b729ac756ce01f5703dcce90c2f9
swe-bench <>
2024-06-12 16:17:09 -0400

    Task instance Python/147 with tests

---
+++ test.py
@@ -0,0 +1,9 @@
+from main import get_max_triples
+def check(get_max_triples):
+
+    assert get_max_triples(5) == 1
+    assert get_max_triples(6) == 4
+    assert get_max_triples(10) == 36
+    assert get_max_triples(100) == 53361
+
+check(get_max_triples)
