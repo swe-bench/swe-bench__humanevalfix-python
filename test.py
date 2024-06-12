b39da6f9403444de34bc4d97e2a2d779735b772b
swe-bench <>
2024-06-12 16:17:01 -0400

    Task instance Python/27 with tests

---
+++ test.py
@@ -0,0 +1,12 @@
+from main import flip_case
+
+
+
+
+
+def check(flip_case):
+    assert flip_case('') == ''
+    assert flip_case('Hello!') == 'hELLO!'
+    assert flip_case('These violent delights have violent ends') == 'tHESE VIOLENT DELIGHTS HAVE VIOLENT ENDS'
+
+check(flip_case)
