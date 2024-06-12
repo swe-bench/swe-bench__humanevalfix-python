ed2b122eb459d497c0b96c36880d0d4cb6382a91
swe-bench <>
2024-06-12 16:17:02 -0400

    Task instance Python/37

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 
 
-def fizz_buzz(n: int):
-    """Return the number of times the digit 7 appears in integers less than n which are divisible by 11 or 13.
-    >>> fizz_buzz(50)
-    0
-    >>> fizz_buzz(78)
-    2
-    >>> fizz_buzz(79)
-    3
+def sort_even(l: list):
+    """This function takes a list l and returns a list l' such that
+    l' is identical to l in the odd indicies, while its values at the even indicies are equal
+    to the values of the even indicies of l, but sorted.
+    >>> sort_even([1, 2, 3])
+    [1, 2, 3]
+    >>> sort_even([5, 6, 3, 4])
+    [3, 6, 5, 4]
     """
-    ns = []
-    for i in range(n):
-        if i % 11 == 0 and i % 13 == 0:
-            ns.append(i)
-    s = ''.join(list(map(str, ns)))
-    ans = 0
-    for c in s:
-        ans += (c == '7')
+    evens = l[::2]
+    odds = l[1::2]
+    odds.sort()
+    ans = []
+    for e, o in zip(evens, odds):
+        ans.extend([e, o])
+    if len(evens) > len(odds):
+        ans.append(evens[-1])
     return ans
-def check(fizz_buzz):
-    assert fizz_buzz(50) == 0
-    assert fizz_buzz(78) == 2
-    assert fizz_buzz(79) == 3
-check(fizz_buzz)
+def check(sort_even):
+    assert tuple(sort_even([1, 2, 3])) == tuple([1, 2, 3])
+    assert tuple(sort_even([5, 6,3,4])) == tuple([3,6,5,4])
+check(sort_even)
