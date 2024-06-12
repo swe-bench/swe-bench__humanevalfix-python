59f0c8d8948febe69087fa7429bdd4661e38844b
swe-bench <>
2024-06-12 16:17:03 -0400

    Task instance Python/67

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 
-def digitSum(s):
-    """Task
-    Write a function that takes a string as input and returns the sum of the upper characters only'
-    ASCII codes.
-
-    Examples:
-        digitSum("") => 0
-        digitSum("abAB") => 131
-        digitSum("abcCd") => 67
-        digitSum("helloE") => 69
-        digitSum("woArBld") => 131
-        digitSum("aAaaaXa") => 153
+def fruit_distribution(s,n):
     """
-    if s == "": return 0
-    return sum(ord(char) if char.islower() else 0 for char in s)
-def check(digitSum):
+    In this task, you will be given a string that represents a number of apples and oranges 
+    that are distributed in a basket of fruit this basket contains 
+    apples, oranges, and mango fruits. Given the string that represents the total number of 
+    the oranges and apples and an integer that represent the total number of the fruits 
+    in the basket return the number of the mango fruits in the basket.
+    for examble:
+    fruit_distribution("5 apples and 6 oranges", 19) ->19 - 5 - 6 = 8
+    fruit_distribution("0 apples and 1 oranges",3) -> 3 - 0 - 1 = 2
+    fruit_distribution("2 apples and 3 oranges", 100) -> 100 - 2 - 3 = 95
+    fruit_distribution("100 apples and 1 oranges",120) -> 120 - 100 - 1 = 19
+    """
+    lis = list()
+    for i in s.split(' '):
+        if i.isdigit():
+            lis.append(int(i))
+    return n - sum(lis) - 1
+def check(fruit_distribution):
     # Check some simple cases
-    assert True, "This prints if this assert fails 1 (good for debugging!)"
-    assert digitSum("") == 0, "Error"
-    assert digitSum("abAB") == 131, "Error"
-    assert digitSum("abcCd") == 67, "Error"
-    assert digitSum("helloE") == 69, "Error"
-    assert digitSum("woArBld") == 131, "Error"
-    assert digitSum("aAaaaXa") == 153, "Error"
-    # Check some edge cases that are easy to work out by hand.
-    assert True, "This prints if this assert fails 2 (also good for debugging!)"
-check(digitSum)
+    assert fruit_distribution("5 apples and 6 oranges",19) == 8
+    assert fruit_distribution("0 apples and 1 oranges",3) == 2
+    assert fruit_distribution("2 apples and 3 oranges",100) == 95
+    assert fruit_distribution("1 apples and 100 oranges",120) == 19
+check(fruit_distribution)
