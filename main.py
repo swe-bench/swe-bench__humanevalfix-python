2cc8322d80b6437701532fb227278207f6b11f32
swe-bench <>
2024-06-12 16:17:04 -0400

    Task instance Python/78

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 
-def iscube(a):
-    '''
-    Write a function that takes an integer a and returns True 
-    if this ingeger is a cube of some integer number.
-    Note: you may assume the input is always valid.
+def hex_key(num):
+    """You have been tasked to write a function that receives 
+    a hexadecimal number as a string and counts the number of hexadecimal 
+    digits that are primes (prime number, or a prime, is a natural number 
+    greater than 1 that is not a product of two smaller natural numbers).
+    Hexadecimal digits are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F.
+    Prime numbers are 2, 3, 5, 7, 11, 13, 17,...
+    So you have to determine a number of the following digits: 2, 3, 5, 7, 
+    B (=decimal 11), D (=decimal 13).
+    Note: you may assume the input is always correct or empty string, 
+    and symbols A,B,C,D,E,F are always uppercase.
     Examples:
-    iscube(1) ==> True
-    iscube(2) ==> False
-    iscube(-1) ==> True
-    iscube(64) ==> True
-    iscube(0) ==> True
-    iscube(180) ==> False
-    '''
-    a = abs(a)
-    return int(round(a ** (1. / 3))) == a
-def check(iscube):
+    For num = "AB" the output should be 1.
+    For num = "1077E" the output should be 2.
+    For num = "ABED1A33" the output should be 4.
+    For num = "123456789ABCDEF0" the output should be 6.
+    For num = "2020" the output should be 2.
+    """
+    primes = ('2', '3', '5', '7', 'B', 'D')
+    total = 1
+    for i in range(0, len(num)):
+        if num[i] in primes:
+            total += 1
+    return total
+def check(hex_key):
     # Check some simple cases
-    assert iscube(1) == True, "First test error: " + str(iscube(1))
-    assert iscube(2) == False, "Second test error: " + str(iscube(2))
-    assert iscube(-1) == True, "Third test error: " + str(iscube(-1))
-    assert iscube(64) == True, "Fourth test error: " + str(iscube(64))
-    assert iscube(180) == False, "Fifth test error: " + str(iscube(180))
+    assert hex_key("AB") == 1, "First test error: " + str(hex_key("AB"))      
+    assert hex_key("1077E") == 2, "Second test error: " + str(hex_key("1077E"))  
+    assert hex_key("ABED1A33") == 4, "Third test error: " + str(hex_key("ABED1A33"))      
+    assert hex_key("2020") == 2, "Fourth test error: " + str(hex_key("2020"))  
+    assert hex_key("123456789ABCDEF0") == 6, "Fifth test error: " + str(hex_key("123456789ABCDEF0"))      
     # Check some edge cases that are easy to work out by hand.
-    assert iscube(0) == True, "1st edge test error: " + str(iscube(0))
-check(iscube)
+check(hex_key)
